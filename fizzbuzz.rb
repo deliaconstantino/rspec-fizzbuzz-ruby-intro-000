0caee287d27b978587d8c1ec68de99a60eff2f27
Delia Constantino <dconstan92@gmail.com>
2020-10-12 18:33:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,21 +1,9 @@
-# def fizzbuzz(int)
-#   if int % 15 == 0
-#     "FizzBuzz"
-#   elsif int % 3 == 0
-#     "Fizz"
-#   elsif int % 5 == 0
-#     "Buzz"
-#   end
-# end
-
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    return "FizzBuzz"
+  if int % 15 == 0
+    "FizzBuzz"
   elsif int % 3 == 0
-    return "Fizz"
+    "Fizz"
   elsif int % 5 == 0
-    return "Buzz"
-  else
-    return nil
+    "Buzz"
   end
 end
