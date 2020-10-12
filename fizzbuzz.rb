20e090767b5659a90956cb485523dc7863071082
Delia Constantino <dconstan92@gmail.com>
2020-10-12 18:23:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,3 +7,15 @@
 #     "Buzz"
 #   end
 # end
+
+def fizzbuzz(int)
+  if (int % 3 && int % 5) == 0
+    return "FizzBuzz"
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  else
+    return "nil"
+  end
+end
